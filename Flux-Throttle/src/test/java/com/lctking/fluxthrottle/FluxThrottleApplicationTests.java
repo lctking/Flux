6572448ccbb0966c6cfe7895f609658f271ea2d6
lctking <1713673102lw@gmail.com>
2025-01-11 20:04:03 +0800

    repository initial

---
+++ Flux-Throttle/src/test/java/com/lctking/fluxthrottle/FluxThrottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lctking.fluxthrottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxThrottleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
