ef7f975c573bed90e86d8df15c6743cc76c7c5b0
lctking <1713673102lw@gmail.com>
2025-01-13 18:51:49 +0800

    更新：实现可选缓存类型服务

--- Flux-Throttle/src/main/java/com/lctking/fluxthrottle/cache/factory/CacheFactory.java
+++ Flux-Throttle/src/main/java/com/lctking/fluxthrottle/cache/factory/CacheFactory.java
@@ -1,15 +1,28 @@
 package com.lctking.fluxthrottle.cache.factory;
 
 import com.lctking.fluxthrottle.cache.impl.DistributeCacheServiceImpl;
+import com.lctking.fluxthrottle.cache.service.CacheService;
 import com.lctking.fluxthrottle.cache.service.DistributeCacheService;
+import com.lctking.fluxthrottle.constant.CacheTypeEnum;
+import java.util.NoSuchElementException;
 import static cn.hutool.extra.spring.SpringUtil.getBean;
+
 public class CacheFactory {
     private static DistributeCacheService DISTRIBUTE_CACHE_SERVICE;
 
     private static final String DISTRIBUTE_CACHE_INITIAL_LOCK = "distribute_cache_initial_lock";
 
 
-    public static DistributeCacheService getInstance(){
+    public static CacheService getInstance(CacheTypeEnum cacheTypeEnum){
+        switch (cacheTypeEnum){
+            case REDIS, DISTRIBUTED -> {
+                return getDistributeCacheService();
+            }
+            default -> throw new NoSuchElementException("no such element:"+cacheTypeEnum);
+        }
+    }
+
+    public static DistributeCacheService getDistributeCacheService(){
         if(DISTRIBUTE_CACHE_SERVICE != null)return DISTRIBUTE_CACHE_SERVICE;
         synchronized (DISTRIBUTE_CACHE_INITIAL_LOCK){
             if(DISTRIBUTE_CACHE_SERVICE != null)return DISTRIBUTE_CACHE_SERVICE;
