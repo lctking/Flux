30d230b55dfeed136d241e8a656c9d8638fb3f99
lctking <1713673102lw@gmail.com>
2025-01-13 17:20:29 +0800

    增加：分布式缓存服务实现类

---
+++ Flux-Throttle/src/main/java/com/lctking/fluxthrottle/cache/impl/DistributeCacheServiceImpl.java
@@ -0,0 +1,32 @@
+package com.lctking.fluxthrottle.cache.impl;
+
+import com.lctking.fluxthrottle.cache.service.DistributeCacheService;
+import com.lctking.fluxthrottle.constant.ThrottleResultEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+public class DistributeCacheServiceImpl implements DistributeCacheService {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private static final String LUA_SCRIPT_RATE_LIMIT_IF_AVAILABLE_PATH = "lua/rate_limit_set_if_available.lua";
+    @Override
+    public Boolean setIfAvailable(String key, long threshold, long windowSize, TimeUnit timeUnit) {
+        DefaultRedisScript<String> redisScript = new DefaultRedisScript<>();
+        ClassPathResource resource = new ClassPathResource(LUA_SCRIPT_RATE_LIMIT_IF_AVAILABLE_PATH);
+        redisScript.setScriptSource(new ResourceScriptSource(resource));
+        redisScript.setResultType(String.class);
+
+        long windowSizeMillis = timeUnit.toMillis(windowSize);
+        long currentTimeMillis = System.currentTimeMillis();
+        String resultStr = stringRedisTemplate.execute(redisScript, List.of(key), String.valueOf(threshold), String.valueOf(windowSizeMillis), String.valueOf(currentTimeMillis));
+        return resultStr != null && !resultStr.equals(ThrottleResultEnum.FAIL.value);
+    }
+}
