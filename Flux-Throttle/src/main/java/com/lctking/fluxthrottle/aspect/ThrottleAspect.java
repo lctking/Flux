37e05220607ec38766ed665d67ee4fa16c8fff55
lctking <1713673102lw@gmail.com>
2025-01-13 22:12:04 +0800

    更新：实现拒绝服务方法的执行

--- Flux-Throttle/src/main/java/com/lctking/fluxthrottle/aspect/ThrottleAspect.java
+++ Flux-Throttle/src/main/java/com/lctking/fluxthrottle/aspect/ThrottleAspect.java
@@ -8,8 +8,17 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import static cn.hutool.extra.spring.SpringUtil.getBean;
+
 @Aspect
 public class ThrottleAspect {
+    private static final Map<String, Object> instanceMap = new HashMap<>();
+
     @Around("@annotation(com.lctking.fluxthrottle.annotation.Throttle)")
     public Object handle(ProceedingJoinPoint joinPoint){
         Throttle throttle = IdempotentGetter(joinPoint);
@@ -21,8 +30,8 @@ public class ThrottleAspect {
             result = joinPoint.proceed();
             instance.postProcess();
         } catch (Throwable e) {
+            result = doReject(throttle);
             instance.exceptionProcess();
-            throw new RuntimeException(e);
         }
         return result;
     }
@@ -31,5 +40,43 @@ public class ThrottleAspect {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         //Method targetMethod = joinPoint.getTarget().getClass().getDeclaredMethod(methodSignature.getName(), methodSignature.getMethod().getParameterTypes());
         return methodSignature.getMethod().getAnnotation(Throttle.class);
+    }
+
+    private static Object doReject(Throttle throttle){
+        String rejectMethod = throttle.rejectMethod();
+        int index = rejectMethod.lastIndexOf(".");
+        String className = rejectMethod.substring(0,index);
+        String methodName = rejectMethod.substring(index+1);
+        Object handlerInstance = getHandlerInstance(className);
+        Object rejectResult = null;
+        try {
+            Method method = handlerInstance.getClass().getMethod(methodName);
+            rejectResult = method.invoke(handlerInstance);
+        } catch (NoSuchMethodException |InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException("fail to do reject");
+        }
+
+        return rejectResult;
+    }
+
+    private static Object getHandlerInstance(String className) {
+        if(instanceMap.containsKey(className)){
+            return instanceMap.get(className);
+        }
+        Object instance = null;
+        try {
+            instance = getBean(className);
+        }catch (Throwable ignored){}
+        if (instance != null)return instance;
+
+        try {
+            Class<?> clazz = Class.forName(className);
+            instance = clazz.getDeclaredConstructor().newInstance();
+        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException("fail to find rejectHandler");
+        }
+        instanceMap.put(className, instance);
+        return instance;
     }
 }
