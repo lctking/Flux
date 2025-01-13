fcea30e1a4b2ddaa013ceb2143897d2f7da04c4f
lctking <1713673102lw@gmail.com>
2025-01-13 18:55:41 +0800

    更新：实现可选基于不同缓存服务的限流执行服务

--- Flux-Throttle/src/main/java/com/lctking/fluxthrottle/executor/ThrottleExecuteFactory.java
+++ Flux-Throttle/src/main/java/com/lctking/fluxthrottle/executor/ThrottleExecuteFactory.java
@@ -1,16 +1,25 @@
 package com.lctking.fluxthrottle.executor;
 
 import com.lctking.fluxthrottle.cache.factory.CacheFactory;
-import com.lctking.fluxthrottle.executor.impl.ThrottleExecuteServiceImpl;
+import com.lctking.fluxthrottle.constant.CacheTypeEnum;
+import com.lctking.fluxthrottle.executor.impl.DistributedThrottleExecuteServiceImpl;
 import com.lctking.fluxthrottle.executor.service.ThrottleExecuteService;
+
+import java.util.NoSuchElementException;
+
 public class ThrottleExecuteFactory {
-    private static final ThrottleExecuteService THROTTLE_EXECUTE_SERVICE;
+    private static final DistributedThrottleExecuteServiceImpl DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
 
     static {
-        THROTTLE_EXECUTE_SERVICE = new ThrottleExecuteServiceImpl(CacheFactory.getInstance());
+        DISTRIBUTED_THROTTLE_EXECUTE_SERVICE = new DistributedThrottleExecuteServiceImpl(CacheFactory.getInstance(CacheTypeEnum.DISTRIBUTED));
     }
 
-    public static ThrottleExecuteService getInstance(){
-        return THROTTLE_EXECUTE_SERVICE;
+    public static ThrottleExecuteService getInstance(CacheTypeEnum cacheTypeEnum){
+        switch (cacheTypeEnum){
+            case REDIS, DISTRIBUTED -> {
+                return DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
+            }
+            default -> throw new NoSuchElementException("no such element:"+cacheTypeEnum);
+        }
     }
 }
