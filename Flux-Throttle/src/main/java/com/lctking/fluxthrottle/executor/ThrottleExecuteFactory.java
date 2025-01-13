104b86387c5efccc42b2dc2bac54013256c22f8e
lctking <1713673102lw@gmail.com>
2025-01-13 19:51:11 +0800

    更新：单例模式懒加载获取分布式限流执行服务类

--- Flux-Throttle/src/main/java/com/lctking/fluxthrottle/executor/ThrottleExecuteFactory.java
+++ Flux-Throttle/src/main/java/com/lctking/fluxthrottle/executor/ThrottleExecuteFactory.java
@@ -3,23 +3,31 @@ package com.lctking.fluxthrottle.executor;
 import com.lctking.fluxthrottle.cache.factory.CacheFactory;
 import com.lctking.fluxthrottle.constant.CacheTypeEnum;
 import com.lctking.fluxthrottle.executor.impl.DistributedThrottleExecuteServiceImpl;
+import com.lctking.fluxthrottle.executor.service.DistributedThrottleExecuteService;
 import com.lctking.fluxthrottle.executor.service.ThrottleExecuteService;
 
 import java.util.NoSuchElementException;
 
 public class ThrottleExecuteFactory {
-    private static final DistributedThrottleExecuteServiceImpl DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
+    private static DistributedThrottleExecuteServiceImpl DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
 
-    static {
-        DISTRIBUTED_THROTTLE_EXECUTE_SERVICE = new DistributedThrottleExecuteServiceImpl(CacheFactory.getInstance(CacheTypeEnum.DISTRIBUTED));
-    }
+    private static final String DISTRIBUTED_THROTTLE_EXECUTE_SERVICE_INITIAL_LOCK = "distributed_throttle_execute_service_initial_lock";
 
     public static ThrottleExecuteService getInstance(CacheTypeEnum cacheTypeEnum){
         switch (cacheTypeEnum){
             case REDIS, DISTRIBUTED -> {
-                return DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
+                return getDistributedThrottleExecuteService();
             }
             default -> throw new NoSuchElementException("no such element:"+cacheTypeEnum);
+        }
+    }
+
+    private static DistributedThrottleExecuteService getDistributedThrottleExecuteService(){
+        if(DISTRIBUTED_THROTTLE_EXECUTE_SERVICE != null)return DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
+        synchronized (DISTRIBUTED_THROTTLE_EXECUTE_SERVICE_INITIAL_LOCK){
+            if(DISTRIBUTED_THROTTLE_EXECUTE_SERVICE != null)return DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
+            DISTRIBUTED_THROTTLE_EXECUTE_SERVICE = new DistributedThrottleExecuteServiceImpl(CacheFactory.getInstance(CacheTypeEnum.DISTRIBUTED));
+            return DISTRIBUTED_THROTTLE_EXECUTE_SERVICE;
         }
     }
 }
